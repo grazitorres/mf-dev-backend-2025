97ed4659c5d64636fdea705c926ea23768751ab2
Graziele <grazieleltorres@gmail.com>
2025-10-07 12:39:45 -0300

    Autenticação e Autorização configuradas

--- mf-dev-backend-2025/Controllers/UsuariosController.cs
+++ mf-dev-backend-2025/Controllers/UsuariosController.cs
@@ -8,9 +8,11 @@ using Microsoft.EntityFrameworkCore;
 using mf_dev_backend_2025.Models;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 
 namespace mf_dev_backend_2025.Controllers
 {
+    [Authorize(Roles ="Admin")]
     public class UsuariosController : Controller
     {
         private readonly AppDbContext _context;
@@ -26,12 +28,20 @@ namespace mf_dev_backend_2025.Controllers
             return View(await _context.Usuarios.ToListAsync());
         }
 
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
         public IActionResult Login()
         {
             return View();  
         }
 
         [HttpPost]
+        [AllowAnonymous]
         public async Task<IActionResult> Login(Usuario usuario)
         {
             var dados = await _context.Usuarios
@@ -79,6 +89,7 @@ namespace mf_dev_backend_2025.Controllers
             return View();
         }
 
+        [AllowAnonymous]
         public async Task<IActionResult> Logout()
         {
             await HttpContext.SignOutAsync();
--- mf-dev-backend-2025/Program.cs
+++ mf-dev-backend-2025/Program.cs
@@ -21,7 +21,7 @@ builder.Services.Configure<CookiePolicyOptions>(options =>
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
-        options.AccessDeniedPath = "/Usuario/AccessDenied/";
+        options.AccessDeniedPath = "/Usuarios/AccessDenied/";
         options.LoginPath = "/Usuarios/Login/";
     });
   
